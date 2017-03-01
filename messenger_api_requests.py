96f228e887668790ea64d7ab9b27d977f66b24df
trainboy2019 <ikerolader@gmail.com>
2017-03-01 08:09:51 -0500

    1stPush

---
+++ messenger_api_requests.py
@@ -0,0 +1,18 @@
+# Script to send messages to FB Messenger via FB Messenger Send API
+import requests
+
+# Constants
+FB_ACCESS_TOKEN = 'YOUR ACCESS TOKEN'
+SEND_API_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + FB_ACCESS_TOKEN
+
+def send_message(messenger_id, text):
+    # Package params into dictionaries for POST request
+    recipient = {'id':messenger_id}
+    message = {'text':text}
+    params = {
+        'recipient':recipient,
+        'message':message
+    }
+
+    # Send POST request to Facebook Messenger Send API to send text message
+    r = requests.post(SEND_API_URL, json=params)
